44b90d9196833c01b5508e1e36a48183b1cbedcf
Barcof <barcof@github.com>
2019-01-15 10:38:29 +0700

    Modify json displayOpenPart, adding field place, accdate and E10 datacode

--- json/displayOpenPart.php
+++ json/displayOpenPart.php
@@ -1,6 +1,17 @@
 <?php
 	include '../connection.php';
 
+	// koneksi ke SVRDBN\JEINSQL2012P
+	$dbasetype = 'odbc_mssql';
+	$user = 'sa';
+	$pass = 'JvcSql@123';
+	$dbase = 'SMTZDBS';
+	$server = "Driver={SQL Server};Server=SVRDBN\JEINSQL2012P;Database=$dbase;";
+
+	$dbnp_conn = ADONewConnection($dbasetype);
+	$dbnp_conn->Connect($server, $user, $pass);
+	// -------------------------------------------------------------------------
+
 	$page = @$_REQUEST['page'] - 1;
 	$limit = @$_REQUEST['limit'];
 	$start = ( $page * $limit ) + 1;
@@ -10,11 +21,12 @@
 
 	$stpartfldsrc = trim(isset($_REQUEST['stpartfldsrc']) ? $_REQUEST['stpartfldsrc'] : '');
 
-	$sql = $conn->Execute("declare @totalcount as int exec displayOpenPart $start, $limit, '{$stpartfldsrc}', @totalcount=@totalcount out");
+	// $sql = $conn->Execute("declare @totalcount as int exec displayOpenPart $start, $limit, '{$stpartfldsrc}', @totalcount=@totalcount out");
 
-	// echo "declare @totalcount as int exec displayDetailPart $start, $limit, '{partno}', @totalcount=@totalcount out";
+	$sql = $conn->Execute("declare @totalcount as int exec displayOpenPart_new $start, $limit, '{$stpartfldsrc}', @totalcount=@totalcount out");
 
-	$totalcount = $sql->fields['9'];
+
+	$totalcount = $sql->fields['12'];
 	$return = array();
 
 		for($i=0;!$sql->EOF;$i++) {
@@ -27,6 +39,9 @@
 			$return[$i]['opendate'] 	= trim($sql->fields['6']);
 			$return[$i]['floorlife'] 	= trim($sql->fields['7']);
 			$return[$i]['nik'] 			= trim($sql->fields['8']);
+			$return[$i]['place']		= trim($sql->fields['9']);
+			$return[$i]['accdate']		= trim($sql->fields['10']);
+			$return[$i]['datacode'] 	= trim($sql->fields['11']);
 
 			$sql->MoveNext();
 		}
